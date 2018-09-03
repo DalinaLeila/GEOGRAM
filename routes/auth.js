02b7780b3725742e416112c34a0c1d79fb5c126f
Dalina <d.weidinger.16@ucl.ac.uk>
2018-09-03 18:35:34 +0200

    routes and layout views updated

--- routes/auth.js
+++ routes/auth.js
@@ -1,78 +1,78 @@
-const express = require('express')
-const passport = require('passport')
-const authRoutes = express.Router()
-const User = require('../models/User')
-const ensureLogin = require('connect-ensure-login')
+const express = require("express");
+const passport = require("passport");
+const authRoutes = express.Router();
+const User = require("../models/User");
+const ensureLogin = require("connect-ensure-login");
 
 // Bcrypt to encrypt passwords
-const bcrypt = require('bcrypt')
-const bcryptSalt = 10
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-authRoutes.get('/login', (req, res, next) => {
-    res.render('auth/login', { message: req.flash('error') })
-})
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
 
 authRoutes.post(
-    '/login',
-    passport.authenticate('local', {
-        successRedirect: 'success',
-        failureRedirect: 'login',
-        failureFlash: true,
-        passReqToCallback: true
-    }),
-    function(req, res) {
-        console.log('req')
-        console.log(res)
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "success",
+    failureRedirect: "login",
+    failureFlash: true,
+    passReqToCallback: true
+  }),
+  function(req, res) {
+    console.log("req");
+    console.log(res);
+  }
+);
+
+authRoutes.get("/success", ensureLogin.ensureLoggedIn(), (req, res, next) => {
+  res.render("index");
+});
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const role = req.body.role;
+  if (email === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate email and password" });
+    return;
+  }
+
+  User.findOne({ email }, "email", (err, user) => {
+    console.log(email);
+    if (user !== null) {
+      res.render("auth/signup", { message: "The email already exists" });
+      return;
     }
-)
 
-authRoutes.get('/success', ensureLogin.ensureLoggedIn(), (req, res, next) => {
-    res.render('index')
-})
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
 
-authRoutes.get('/signup', (req, res, next) => {
-    res.render('auth/signup')
-})
+    const newUser = new User({
+      email,
+      password: hashPass
+      // role: 'teacher'
+    });
 
-authRoutes.post('/signup', (req, res, next) => {
-    const email = req.body.email
-    const password = req.body.password
-    const role = req.body.role
-    if (email === '' || password === '') {
-        res.render('auth/signup', { message: 'Indicate email and password' })
-        return
-    }
+    newUser.save(err => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+        console.log(err);
+      } else {
+        res.redirect("/auth/login");
+      }
+    });
+  });
+});
 
-    User.findOne({ email }, 'email', (err, user) => {
-        console.log(email)
-        if (user !== null) {
-            res.render('auth/signup', { message: 'The email already exists' })
-            return
-        }
+authRoutes.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-        const salt = bcrypt.genSaltSync(bcryptSalt)
-        const hashPass = bcrypt.hashSync(password, salt)
-
-        const newUser = new User({
-            email,
-            password: hashPass
-            // role: 'teacher'
-        })
-
-        newUser.save(err => {
-            if (err) {
-                res.render('auth/signup', { message: 'Something went wrong' })
-                console.log(err)
-            } else {
-                res.redirect('/auth/login')
-            }
-        })
-    })
-})
-
-authRoutes.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/')
-})
-
-module.exports = authRoutes
+module.exports = authRoutes;
--- routes/creator.js
+++ routes/creator.js
@@ -3,72 +3,67 @@ const creator = express.Router();
 const User = require("../models/User");
 const Game = require("../models/Game");
 const Task = require("../models/Task");
+const ensureLogin = require("connect-ensure-login");
 
-creator.get("/creator-overview", (req, res, next) => {
-  res.render("creator/creator-overview");
-});
+creator.get(
+  "/creator-overview",
+  ensureLogin.ensureLoggedIn(),
+  (req, res, next) => {
+    res.render("creator/creator-overview");
+  }
+);
 
-creator.get("/game-details", (req, res, next) => {
+creator.get("/game-details", ensureLogin.ensureLoggedIn(), (req, res, next) => {
   res.render("creator/game-details");
 });
 
 creator.post("/game-details", (req, res, next) => {
+  const { title, description, private } = req.body;
 
-    const { title,
-        description,
-        private } = req.body
+  console.log(req.body);
 
-        console.log(req.body)
-
-    const game = new Game({
-        title: title,
-        creator: req.user._id,
-        private: private,
-        description: description,
-    }).save().then(
-        game => {
-            let id = game._id
-            console.log("ID of Game: ", id)
-            res.render("creator/tasks-overview", {id})
-        });
-})
+  const game = new Game({
+    title: title,
+    creator: req.user._id,
+    private: private,
+    description: description
+  })
+    .save()
+    .then(game => {
+      let id = game._id;
+      console.log("ID of Game: ", id);
+      res.render("creator/tasks-overview", { id });
+    });
+});
 
 creator.get("/add-task/:id", (req, res, next) => {
-    let id = req.params.id;
-    res.render("creator/add-task", {id});
-})
-
+  let id = req.params.id;
+  res.render("creator/add-task", { id });
+});
 
 creator.post("/add-task/:id", (req, res, next) => {
-    let id = req.params.id;
-    const { title,
-        location,
-        taskType,
-        description } = req.body
+  let id = req.params.id;
+  const { title, location, taskType, description } = req.body;
 
-    const task = new Task({
-        title: title,
-        taskType: taskType,
-        description: description,
-        location: location
-    }).save().then(
-        task => {
-            Game.findByIdAndUpdate(id, {$push: { tasks: task._id}}).then(game=>{
-                res.render("creator/tasks-overview", { game })
-            })
-            
-        });
-})
+  const task = new Task({
+    title: title,
+    taskType: taskType,
+    description: description,
+    location: location
+  })
+    .save()
+    .then(task => {
+      Game.findByIdAndUpdate(id, { $push: { tasks: task._id } }).then(game => {
+        res.render("creator/tasks-overview", { game });
+      });
+    });
+});
 
 creator.get("/tasks-overview/:id", (req, res, next) => {
-    let id = req.params.id;
-    game= Game.findById(id).then(game=>{
-        res.render("creator/tasks-overview", {game});
-    })
-    
-})
-
-
-
+  let id = req.params.id;
+  game = Game.findById(id).then(game => {
+    res.render("creator/tasks-overview", { game });
+  });
+});
 
 module.exports = creator;
