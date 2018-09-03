649fb64cba74669734343a5e541708d7b9146c02
Dalina <d.weidinger.16@ucl.ac.uk>
2018-09-03 15:32:43 +0200

    routes/auth.js update

--- routes/auth.js
+++ routes/auth.js
@@ -1,73 +1,73 @@
-const express = require('express')
-const passport = require('passport')
-const authRoutes = express.Router()
-const User = require('../models/User')
+const express = require("express");
+const passport = require("passport");
+const authRoutes = express.Router();
+const User = require("../models/User");
 
 // Bcrypt to encrypt passwords
-const bcrypt = require('bcrypt')
-const bcryptSalt = 10
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
 
-authRoutes.get('/login', (req, res, next) => {
-    res.render('auth/login', { message: req.flash('error') })
-})
+authRoutes.get("/login", (req, res, next) => {
+  res.render("auth/login", { message: req.flash("error") });
+});
 
 authRoutes.post(
-    '/login',
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/auth/login',
-        failureFlash: true,
-        passReqToCallback: true
-    }),
-    function(req, res) {
-        console.log('req')
-        console.log(res)
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/auth/login",
+    failureFlash: true,
+    passReqToCallback: true
+  }),
+  function(req, res) {
+    console.log("req");
+    console.log(res);
+  }
+);
+
+authRoutes.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+authRoutes.post("/signup", (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const role = req.body.role;
+  if (email === "" || password === "") {
+    res.render("auth/signup", { message: "Indicate email and password" });
+    return;
+  }
+
+  User.findOne({ email }, "email", (err, user) => {
+    console.log(email);
+    if (user !== null) {
+      res.render("auth/signup", { message: "The email already exists" });
+      return;
     }
-)
 
-authRoutes.get('/signup', (req, res, next) => {
-    res.render('auth/signup')
-})
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
 
-authRoutes.post('/signup', (req, res, next) => {
-    const email = req.body.email
-    const password = req.body.password
-    const role = req.body.role
-    if (email === '' || password === '') {
-        res.render('auth/signup', { message: 'Indicate email and password' })
-        return
-    }
+    const newUser = new User({
+      email,
+      password: hashPass
+      // role: 'teacher'
+    });
 
-    User.findOne({ email }, 'email', (err, user) => {
-        console.log(email)
-        if (user !== null) {
-            res.render('auth/signup', { message: 'The email already exists' })
-            return
-        }
+    newUser.save(err => {
+      if (err) {
+        res.render("auth/signup", { message: "Something went wrong" });
+        console.log(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+});
 
-        const salt = bcrypt.genSaltSync(bcryptSalt)
-        const hashPass = bcrypt.hashSync(password, salt)
+authRoutes.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
-        const newUser = new User({
-            email,
-            password: hashPass
-            // role: 'teacher'
-        })
-
-        newUser.save(err => {
-            if (err) {
-                res.render('auth/signup', { message: 'Something went wrong' })
-                console.log(err)
-            } else {
-                res.redirect('/')
-            }
-        })
-    })
-})
-
-authRoutes.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/')
-})
-
-module.exports = authRoutes
+module.exports = authRoutes;
