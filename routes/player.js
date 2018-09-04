fc37c49c6a88796ffd19f3d0c21e84a0d2533485
Sean Walsh <scjwalsh@gmail.com>
2018-09-04 18:01:59 +0200

    file upload to cloudinary works on player-task page- player.js post route

--- routes/player.js
+++ routes/player.js
@@ -5,6 +5,7 @@ const Game = require('../models/Game')
 const Task = require('../models/Task')
 const ensureLogin = require('connect-ensure-login')
 const { upload } = require('../utils/cloudinary')
+const fs = require('fs')
 
 player.get('/player-overview', (req, res) => {
     Game.find({}).then(game => {
@@ -22,46 +23,47 @@ player.get('/game/:id', ensureLogin.ensureLoggedIn(), (req, res) => {
 
 player.get('/player-task/:id', ensureLogin.ensureLoggedIn(), (req, res) => {
     const id = req.params.id
+    let tasks = []
 
-    Game.findById(id).then(game => {
-        let tasks = []
-        const taskRefs = game.tasks
-
-        taskRefs.forEach(taskRef => {
-            Task.findById(taskRef).then(task => {
-                // console.log(task)    //returns obj
-                tasks.push(task)
+    Game.findById(id)
+        .then(game => {
+            const taskRefs = game.tasks
+            taskRefs.forEach(taskRef => {
+                Task.findById(taskRef).then(task => {
+                    // console.log(task) //returns obj
+                    tasks.push(task)
+                })
             })
         })
-        res.render('player/player-task', { tasks })
-    })
-
-    // .then(el => {
-    //     console.log(tasks) //empty array
-    //     res.render('player/player-task', { task })
-    // })
+        .then(() => {
+            // console.log(tasks)   //returns empty array???
+            //***how to only show current task????***
+            res.render('player/player-task', { tasks })
+        })
 })
 
 player.post('/player-task/:id', (req, res, next) => {
-    // const id = req.params.id
-    // console.log(req.files)
-    // const { file } = req.files
-    // // const { name } = req.body
-    // const path = `public/uploads/${file.name}`
-    // file.mv(path, function(err) {
-    //     if (err) return res.status(500).send(err)
-    //     let taskId
-    //     Task.findByIdAndUpdate(
-    //         id,
-    //         {
-    //             file: result.url
-    //         },
-    //         { new: true }
-    //     ).then(result => {
-    //         return upload(path) //????
-    //         res.send(result)
-    //     })
-    // })
+    const id = req.params.id
+    const { file } = req.files
+    // const { name } = req.body
+    const path = `public/uploads/${file.name}`
+
+    file.mv(path, function(err) {
+        if (err) return res.status(500).send(err)
+
+        //uploads to cloudinary
+        upload(path)
+            .then(result => {
+                // console.log('RESUOLT', result)
+                // console.log('REQ>BODY: ', req.body.file)
+                return Task.findByIdAndUpdate(id, { file: req.body.file }, { new: true, upsert: true })
+            })
+            .then(task => {
+                //deletes local copy
+                fs.unlinkSync(path)
+                res.send(task)
+            })
+    })
 })
 
 player.get('/finished', ensureLogin.ensureLoggedIn(), (req, res) => {
