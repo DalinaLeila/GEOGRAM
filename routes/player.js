11795e84f9c91ee353cf7b70b73edee03d3438c6
Dalina <d.weidinger.16@ucl.ac.uk>
2018-09-04 14:31:35 +0200

    new links nav bar

--- routes/player.js
+++ routes/player.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const player = express.Router()
-const User = require('../models/User')
-const Game = require('../models/Game')
-const Task = require('../models/Task')
-const ensureLogin = require('connect-ensure-login')
+const express = require("express");
+const player = express.Router();
+const User = require("../models/User");
+const Game = require("../models/Game");
+const Task = require("../models/Task");
+const ensureLogin = require("connect-ensure-login");
 
-player.get('/overview', ensureLogin.ensureLoggedIn(), (req, res) => {
-    res.render('player/player-overview')
-})
+player.get("/player-overview", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("player/player-overview");
+});
 
-player.get('/task', ensureLogin.ensureLoggedIn(), (req, res) => {
-    res.render('player/player-task')
-})
+player.get("/task", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("player/player-task");
+});
 
-player.get('/finished', ensureLogin.ensureLoggedIn(), (req, res) => {
-    res.render('player/game-finished')
-})
+player.get("/finished", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("player/game-finished");
+});
 
-module.exports = player
+module.exports = player;
