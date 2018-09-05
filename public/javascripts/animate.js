ef8c12a86f7a50aceb3e0e5e30c0b536e5418298
Sean Walsh <scjwalsh@gmail.com>
2018-09-05 10:59:49 +0200

    added styles and animations, added new css file - animate.css

---
+++ public/javascripts/animate.js
@@ -0,0 +1,12 @@
+const btnLogin = document.querySelector('button')
+const body = document.querySelector('body')
+// const btnNewGame = document.querySelector('.btn-newGame')
+
+btnLogin.addEventListener('click', () => {
+    body.classList.add('fade-out')
+})
+
+// btnNewGame.addEventListener('click', () => {
+//     body.classList.add('fade-out')
+//     prompt('btn clicked')
+// })
