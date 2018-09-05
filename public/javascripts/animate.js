ee97851d1a4d74ec3cc09d1dc7041b266221d98b
Sean Walsh <scjwalsh@gmail.com>
2018-09-05 16:18:19 +0200

    styling creator pages

--- public/javascripts/animate.js
+++ public/javascripts/animate.js
@@ -1,6 +1,7 @@
 const link = document.querySelector('.link-create')
 const body = document.querySelector('body')
 const btnLogin = document.querySelector('.btn-login')
+const btnNewGame = document.querySelector('a.animate-link')
 
 link.addEventListener('click', evt => {
     evt.preventDefault()
@@ -11,11 +12,15 @@ link.addEventListener('click', evt => {
     }, 500)
 })
 
-btnLogin.addEventListener('click', evt => {
+btnNewGame.addEventListener('click', evt => {
     evt.preventDefault()
     body.classList.add('fade-out')
 
     setTimeout(() => {
-        window.location = btnLogin.href
+        window.location = btnNewGame.href
     }, 500)
 })
+
+// btn.addEventListener('click', () => {
+//     body.classList.add('fade-out')
+// })
