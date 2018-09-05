6e750ed5932b685d1cf974f1c91e63b6d134ccaa
Sean Walsh <scjwalsh@gmail.com>
2018-09-05 14:43:21 +0200

    fade out animation works for creator-overview

--- public/javascripts/animate.js
+++ public/javascripts/animate.js
@@ -1,12 +1,21 @@
-const btnLogin = document.querySelector('button')
+const link = document.querySelector('.link-create')
 const body = document.querySelector('body')
-const btnNewGame = document.querySelector('.btn-newGame')
+const btnLogin = document.querySelector('.btn-login')
 
-btnLogin.addEventListener('click', () => {
+link.addEventListener('click', evt => {
+    evt.preventDefault()
     body.classList.add('fade-out')
+
+    setTimeout(() => {
+        window.location = link.href
+    }, 500)
 })
 
-// btnNewGame.addEventListener('click', () => {
-//     alert('btn clicked')
-//     btnNewGame.classList.add('fade-out')
-// })
+btnLogin.addEventListener('click', evt => {
+    evt.preventDefault()
+    body.classList.add('fade-out')
+
+    setTimeout(() => {
+        window.location = btnLogin.href
+    }, 500)
+})
