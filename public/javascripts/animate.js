a2e42885033b6b9e9f61d383a2f95692453e1930
Sean Walsh <scjwalsh@gmail.com>
2018-09-05 12:38:54 +0200

    working on creator-overview style

--- public/javascripts/animate.js
+++ public/javascripts/animate.js
@@ -1,12 +1,12 @@
 const btnLogin = document.querySelector('button')
 const body = document.querySelector('body')
-// const btnNewGame = document.querySelector('.btn-newGame')
+const btnNewGame = document.querySelector('.btn-newGame')
 
 btnLogin.addEventListener('click', () => {
     body.classList.add('fade-out')
 })
 
 // btnNewGame.addEventListener('click', () => {
-//     body.classList.add('fade-out')
-//     prompt('btn clicked')
+//     alert('btn clicked')
+//     btnNewGame.classList.add('fade-out')
 // })
