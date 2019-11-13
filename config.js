24cc1126a9345850698481286d59cfae21fedb8a
Dalina Weidinger <dalina.weidinger@gmail.com>
2019-11-13 16:42:11 +0100

    deleted config

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT || 3000,
-    MONGODB_URI:
-    process.env.MONGODB_URI || "mongodb://localhost/project2",
-    CLOUDINARY_NAME: 'dvmesdtwc',
-    CLOUDINARY_KEY: '833691355391115',
-    CLOUDINARY_SECRET: 'UecTB-WO4kzzNfl950xc5ipl0e8'
-}
